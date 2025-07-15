6f27575ff282e06c24fc0d7bdb9c73ce935825e2
hash-luk <lucas.patrick2506@gmail.com>
2025-07-14 21:05:27 -0300

    feat: Linked register screen to api

--- front-end/src/app/components/auth/RegisterForm.tsx
+++ front-end/src/app/components/auth/RegisterForm.tsx
@@ -1,21 +1,51 @@
 import { LockKeyhole, Mail, User } from "lucide-react";
 import InputField from "../ui/Input";
 import Button from "../ui/Button";
+import { useRegister } from "@/app/hooks/useRegister";
+import { useState } from "react";
 
 interface RegisterFormProps {
   onToggleMode: () => void;
 }
 
 export default function RegisterForm({ onToggleMode }: RegisterFormProps) {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const register = useRegister();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await register.mutateAsync(form);
+      alert("Usuário registrado com sucesso");
+      onToggleMode();
+    } catch (error) {
+      console.error("Erro ao registrar:", error);
+    }
+  };
+
   return (
     <>
-      <form className="flex flex-col justify-center gap-4">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center gap-4"
+      >
         <InputField
           inputID="name"
           inputName="name"
           inputPlaceholder="Enter your username"
           label="Username"
           icon={User}
+          onChange={handleChange}
         />
         <InputField
           inputID="email"
@@ -23,6 +53,7 @@ export default function RegisterForm({ onToggleMode }: RegisterFormProps) {
           inputPlaceholder="Enter your email"
           label="Email"
           icon={Mail}
+          onChange={handleChange}
         />
         <InputField
           inputID="password"
@@ -31,6 +62,7 @@ export default function RegisterForm({ onToggleMode }: RegisterFormProps) {
           inputType="password"
           label="Password"
           icon={LockKeyhole}
+          onChange={handleChange}
         />
         <Button buttonType="submit" buttonText="Sign up" />
       </form>
