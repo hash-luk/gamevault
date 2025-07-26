da641abcd62d9f4e19e469cf13fd83b547360c6f
hash-luk <lucas.patrick2506@gmail.com>
2025-07-26 15:39:08 -0300

    refactor: Changed Header component folder location

--- front-end/src/app/(auth)/layout.tsx
+++ front-end/src/app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import Header from "../components/ui/Header";
+import Header from "../components/structure/Header";
 
 export default function AuthenticatedLayout({
   children,
